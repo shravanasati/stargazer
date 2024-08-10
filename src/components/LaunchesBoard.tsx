5981e0ea538cc870213bfaa881334dda11329752
Shravan Asati <dev.shravan@protonmail.com>
2024-08-10 17:15:05 +0530

    card hover for launches

--- src/components/LaunchesBoard.tsx
+++ src/components/LaunchesBoard.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { Loading } from "./Loading"
+import { HoverEffect } from "./ui/card-hover-effect"
+
 
 export function LaunchesBoard() {
   const [element, setElement] = useState(Loading())
@@ -7,19 +9,8 @@ export function LaunchesBoard() {
     const fetchLaunches = async () => {
       const response = await fetch("http://localhost:5000/api/launches")
       const data = await response.json()
-      console.log(data)
       setElement(
-        <div className="m-2 p-2">
-          {data.map((entry: any) => {
-            return (
-              <>
-              <h2 className="text-3xl my-4 font-bold text-black">{entry.name}</h2>
-              <h3 className="text-3xl my-4 font-bold text-black">{entry.rocket}</h3>
-              <p className="text-black">{entry.time}</p>
-              </>
-            )
-          })}
-        </div>
+        <HoverEffect items={data} />
       )
     }
 
