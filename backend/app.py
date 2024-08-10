13bd1061a9ad20b7f9bb5da46f2d57b94c145e18
AnshMNSoni <anshsoni702@gmail.com>
2024-08-10 13:30:11 +0530

    Spacedevs content

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from flask import Flask, send_from_directory
+from apis.spacedevs import SpacedevsAPI
 
 app = Flask(__file__)
 dist_dir = Path(__file__).parent.parent / "dist"
 app.static_folder = str(dist_dir)
 assets_folder = str(dist_dir / "assets")
+spacedevs_client = SpacedevsAPI()
 
 
 @app.get("/")
@@ -16,9 +18,16 @@ def index():
 def assets(path):
     return send_from_directory(app.static_folder, path)
 
+
 @app.get("/events")
 def events():
-    pass
+    return spacedevs_client.events()
+
+
+@app.get("/launches")
+def launches():
+    return spacedevs_client.launches()
+
 
 @app.get("/ping")
 def ping():
