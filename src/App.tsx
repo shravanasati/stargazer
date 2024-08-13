4f27542f0d4b5224f51e0519438dff4e4890feae
ni3rav <mahetan35@gmail.com>
2024-08-13 16:33:00 +0530

    added montserrat font and started to fix home page, next is bento grid

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import { Button } from "@/components/ui/button";
 type Props = {
   text: string;
   className?: string;
-  to: string; // Change href to to
+  to: string; 
 };
 
 export default function App() {
   return (
     <>
-      <div className="h-screen relative w-full bg-zinc-950 flex flex-col items-center justify-center overflow-scroll">
+      <div className="h-screen relative w-full bg-zinc-950 flex flex-col items-center justify-center overflow-hidden">
         <div className="w-full h-screen absolute inset-0">
           <SparklesCore
             id="tsparticlesfullpage"
@@ -26,11 +26,11 @@ export default function App() {
             particleColor="#cbd5e1"
           />
         </div>
-        <div className="text-zinc-50 w-4/5 h-screen z-50 flex flex-col justify-center items-center gap-4 flex-wrap">
-          <h1 className="text-6xl md:text-7xl p-2 text-center w-full mb-4">
+        <div className="text-zinc-100 w-full h-screen z-50 flex flex-col justify-center items-center gap-2 flex-wrap">
+          <h1 className="text-7xl p-6 text-center w-full font-black md:p-0">
             Welcome to Stargazer
           </h1>
-          <p className="w-full md:w-1/2 text-center md:text-lg text-md text-slate-100/70">
+          <p className="w-full md:w-1/2 text-center md:text-lg text-xl text-slate-100/70 font-light opacity-90 ">
             Unveil the mysteries of the night sky with a dashboard designed for
             dreamers and explorers. Your journey to the stars starts here.
           </p>
@@ -46,7 +46,7 @@ export function CtaButton({ text, className, to }: Props) {
   return (
     <Link to={to}>
       <Button
-        className={`${className} hover:bg-zinc-800 text-xl p-6 shadow-2xl shadow-blue-500/50 hover:shadow-indigo-500/50 transition-all ease-in-out duration-500 hover:shadow-lg`}
+        className={`${className} hover:bg-transparent text-xl p-6 shadow-2xl shadow-blue-500/50 hover:shadow-indigo-500/50 transition-all ease-in-out duration-500 hover:shadow-lg hover:text-white hover:border-white hover:border border-0`}
       >
         {text}
       </Button>
